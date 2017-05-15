4d4678430b93195e4947feb78f891e3a3fffeded
高天阳 <gaotianyang@haomo-studio.com>
2017-05-15 17:03:29 +0800

    ES6test

--- src/js/ES6test.js
+++ src/js/ES6test.js
@@ -40,17 +40,38 @@
 //NO.14
 
 //NO.15
+// let obj = {
+//     name: "江上",
+//     age: '',
+//     saySelf: () => {
+//         console.log(this.name)
+//     }
+// };
+// obj.saySelf();          //报错
+// console.log(obj.name);  //江上
+// console.log(obj.age);   //''
+// babel这里有bug this.name<=>window.name被识别成了undefined.name
 
 //NO.16
-
 //关于Object.assign的详细讲解    http://blog.csdn.net/qq_30100043/article/details/53422657
-//NO.17     下面代码中，v1、v2、v3分别是字符串、布尔值和数值，结果只有字符串合入目标对象（以字符数组的形式），数值和布尔值都会被忽略。这是因为只有字符串的包装对象，会产生可枚举属性。
-var v1 = 'abc';
-var v2 = true;
-var v3 = 10;
+// var target = { a: 1 };
+//
+// var source1 = { b: 2 };
+// var source2 = { c: 3 };
+//
+// Object.assign(target, source1, source2);
+// console.log(target);
 
-var obj = Object.assign({}, v1, v2, v3);
-console.log(obj);
+//NO.17
+// var v1 = 'abc';
+// var v2 = true;
+// var v3 = 10;
+// var obj = Object.assign({}, v1, v2, v3);
+// console.log(obj);
+//
+// 上面代码中，v1、v2、v3分别是字符串、布尔值和数值，
+// 结果只有字符串合入目标对象（以字符数组的形式），数值和布尔值都会被忽略。
+// 这是因为只有字符串的包装对象，会产生可枚举属性。
 
 //NO.18
 // var objA = {a:1};
@@ -77,11 +98,9 @@ console.log(obj);
 // let {a, b , ...c} = {a:1, b: 2, d:3,e:4}
 // console.log(c);
 
-
 //NO.21
 // let a = {a: 1, b:'abc'};
 // let c = {...a};
 // console.log(c);
-
 
 //NO.22
