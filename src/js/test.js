1acf7b3b521cb5dc9c9e4e5a0f05a15dfb9485d3
高天阳 <gaotianyang@haomo-studio.com>
2017-05-15 12:02:34 +0800

    init

--- src/js/test.js
+++ src/js/test.js
@@ -1,11 +1,4 @@
 /**
  * Created by gaotianyang on 2017/5/11 14:03
  **/
-
-{
-    let a = 10;
-    var b = 1;
-}
-
-console.log(a); // ReferenceError: a is not defined.
-console.log(b);  // 1
+console.log('\u{1F680}' === '\uD83D\uDE80');    // true
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 /**
  * Created by gaotianyang on 2017/5/11 13:55
  **/
-let a = 1;
+
+{
+    let a = 10;
+    var b = 1;
+}
+let c = 1;
+
+console.log(a); // ReferenceError: a is not defined.
+console.log(b);  // 1
+console.log(c);  // 1
 
 var selected = allJobs.filter(job => job.isSelected());
